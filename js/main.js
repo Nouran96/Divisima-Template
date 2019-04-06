390f20dda121b5e9903072e9f70bef7aebd60cc4
Nouran <only1nour@ymail.com>
2019-04-06 10:37:48 +0200

    Initial Setup of Page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        autoplay: true
+    });
+});
+
+var owl = $('.owl-carousel');
+
+$('.owl-prev').click(function(){
+    owl.trigger('prev.owl.carousel');
+});
+
+$('.owl-next').click(function(){
+    owl.trigger('next.owl.carousel');
+});
